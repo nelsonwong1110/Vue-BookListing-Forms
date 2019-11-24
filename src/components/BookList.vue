b5e536897d3098ab7dc69d84220e8406d0cc3307
nelsonwong1110 <nelsonwong1110@gmail.com>
2019-11-24 19:18:27 +0800

    done

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,12 +4,21 @@
     <ul>
       <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
     </ul>
+    <hr>
+    <h2>Filtered Books By Ownership</h2>
+    <select v-model="holding"> 
+      <option v-for="filter in filters">{{filter}}</option>
+    </select>
+    <ul>
+      <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
+    </ul>
     <br><hr>
     <book-form @addBook='appendBook'></book-form>
   </div>
 </template>
 
 <script>
+import _ from "lodash";
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
 
@@ -20,11 +29,18 @@ export default {
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
       books: [
-        {finishedReading:true, title: "Self-Reliance", author: "Ralph Waldo Emerson" },
-        {finishedReading:false, title: "American Gods", author: "Neil Gaiman" },
-        {finishedReading:true, title: "Amusing Ourselves to Death", author: "Neil Postman" }
-      ]
+        {finishedReading:true, title: "Self-Reliance", author: "Ralph Waldo Emerson", ownership:["borrowed"] },
+        {finishedReading:false, title: "American Gods", author: "Neil Gaiman", ownership:["bought"] },
+        {finishedReading:true, title: "Amusing Ourselves to Death", author: "Neil Postman", ownership:["borrowed"] }
+      ],
+      filters: ["bought", "borrowed"],
+      holding : "bought"
     };
+  },
+  computed:{
+    filteredBooks() {
+      return _.filter(this.books, ["ownership", this.holding]);
+    }
   },
   components: {
     BookItem,
@@ -32,7 +48,7 @@ export default {
   },
   methods: {
     appendBook(bookData) {
-      this.books.push({ finishedReading:bookData.finishedReading, title: bookData.bookTitle, author: bookData.bookAuthor });
+      this.books.push({ finishedReading:bookData.finishedReading, title: bookData.bookTitle, author: bookData.bookAuthor, ownership:bookData.ownership });
     }
   }
 };
